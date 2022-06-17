31eda31cb32f47ac5ab30cda1a90c1da7c4c6e99
肖龙威 <704926476@qq.com>
2022-06-17 17:46:02 +0800

    适配器设计模式

--- src/main/java/org/test/design/adapter/VoltageAdapter.java
+++ src/main/java/org/test/design/adapter/VoltageAdapter.java
@@ -3,7 +3,7 @@ package org.test.design.adapter;
 import lombok.AllArgsConstructor;
 
 /**
- * 适配器模式:将目标数据转换成需要的数据
+ * 适配器模式:将目标数据转换成需要的数据(将不兼容的数据转换成兼容的数据)
  * @author 肖龙威
  * @date 2021/04/29 14:27
  */
