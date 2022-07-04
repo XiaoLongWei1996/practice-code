a3ff1e32e47a42ae1bcc243248a729bcb713d843
肖龙威 <704926476@qq.com>
2022-07-04 15:01:22 +0800

    装饰设计模式

--- src/main/java/org/test/design/decorator/Drink.java
+++ src/main/java/org/test/design/decorator/Drink.java
@@ -1,7 +1,5 @@
 package org.test.design.decorator;
 
-import lombok.AllArgsConstructor;
-
 /**
  * 装饰者设计模式:在不改变对象原有的结构上增加代码功能
  * 也就是把一个对象作为另一个对象的属性,层层包含
@@ -9,12 +7,16 @@ import lombok.AllArgsConstructor;
  * @author 肖龙威
  * @date 2021/04/29 15:39
  */
-@AllArgsConstructor
 public abstract class Drink {
 
     protected Integer price;
 
     protected Drink drink;
+
+    public Drink(Integer price, Drink drink) {
+        this.price = price;
+        this.drink = drink;
+    }
 
     public Integer allPrice(){
         if (drink == null){
