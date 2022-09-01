7f374b5da9ad14d466f131078fda3369a41560ca
给你new对象 <53401233+XiaoLongWei1996@users.noreply.github.com>
2022-09-01 17:16:20 +0800

    Delete src directory

--- src/main/java/com/test/springboot/domain/Parent.java
+++
@@ -1,8 +0,0 @@
-package com.test.springboot.domain;
-
-/**
- * @author 肖龙威
- * @date 2022/04/14 11:21
- */
-public class Parent {
-}
--- src/main/resources/static/windows.bat
+++
@@ -1,3 +0,0 @@
-@echo "parse %2 for thumbnails"
-@echo "save to %1"
-dplayer-thumbnails -o %1 -q 60 %2
