800a5b4dd24388e4910ef2e3c989e213b54cdaab
肖龙威 <704926476@qq.com>
2022-10-29 22:27:12 +0800

    alibaba

--- alibaba-config/src/main/java/com/springcloud/test/alibabaconfig/controller/ConfigController.java
+++ alibaba-config/src/main/java/com/springcloud/test/alibabaconfig/controller/ConfigController.java
@@ -1,6 +1,7 @@
 package com.springcloud.test.alibabaconfig.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author 肖龙威
  * @date 2022/10/28 22:20
  */
+@RefreshScope
 @RestController
 @RequestMapping("config")
 public class ConfigController {
@@ -17,8 +19,13 @@ public class ConfigController {
     @Value("${a}")
     private String a;
 
+    @Value("${b}")
+    private String b;
+
     @GetMapping("test")
     public ResponseEntity<String> test() {
-        return ResponseEntity.ok(a);
+        System.out.println(a);
+        System.out.println(b);
+        return ResponseEntity.ok(b);
     }
 }
