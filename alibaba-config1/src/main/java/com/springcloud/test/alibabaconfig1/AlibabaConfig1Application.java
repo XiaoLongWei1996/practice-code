63ec9f22e0c2b80aba659441ddb97cf548645cb5
肖龙威 <704926476@qq.com>
2022-10-31 14:28:42 +0800

    alibaba

---
+++ alibaba-config1/src/main/java/com/springcloud/test/alibabaconfig1/AlibabaConfig1Application.java
@@ -0,0 +1,13 @@
+package com.springcloud.test.alibabaconfig1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabaConfig1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaConfig1Application.class, args);
+    }
+
+}
---
+++ alibaba-config1/src/test/java/com/springcloud/test/alibabaconfig1/AlibabaConfig1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.test.alibabaconfig1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlibabaConfig1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
