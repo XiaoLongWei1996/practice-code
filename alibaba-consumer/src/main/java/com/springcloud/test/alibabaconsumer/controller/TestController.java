c3a82598f931804dab98915ff5a91592c3e2885e
肖龙威 <704926476@qq.com>
2022-11-01 17:33:00 +0800

    alibaba

--- alibaba-consumer/src/main/java/com/springcloud/test/alibabaconsumer/controller/TestController.java
+++ alibaba-consumer/src/main/java/com/springcloud/test/alibabaconsumer/controller/TestController.java
@@ -1,5 +1,11 @@
 package com.springcloud.test.alibabaconsumer.controller;
 
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.context.ContextUtil;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRule;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author 肖龙威
@@ -23,9 +32,36 @@ public class TestController {
     @Resource
     private DiscoveryClient discoveryClient;
 
+    @PostConstruct
+    public void init() {
+        //定义规则
+        List<DegradeRule> rules = new ArrayList<>();
+        DegradeRule rule = new DegradeRule();
+        rule.setResource("hello");
+        rule.setMinRequestAmount(5);
+        rule.setSlowRatioThreshold(0.5);
+
+    }
+
+    @SentinelResource("query")
     @GetMapping("query")
     public ResponseEntity<String> test() {
-        String result = restTemplate.getForObject("http://xxl_config/config/test", String.class);
+        String result = restTemplate.getForObject("http://config/config/test", String.class);
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("query1")
+    public ResponseEntity<String> test1() {
+        Entry entry = null;
+        try {
+            entry = SphU.entry("hello");
+            System.out.println("hello");
+        } catch (BlockException e) {
+            e.printStackTrace();
+        } finally {
+            entry.exit();
+            ContextUtil.exit();
+        }
+        return ResponseEntity.ok("result");
     }
 }
