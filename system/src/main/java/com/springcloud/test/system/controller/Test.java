dd08b46e26770705dcf38b80209573631d0d133b
肖龙威 <704926476@qq.com>
2023-02-06 15:45:57 +0800

    minio

--- system/src/main/java/com/springcloud/test/system/controller/Test.java
+++ system/src/main/java/com/springcloud/test/system/controller/Test.java
@@ -1,12 +1,10 @@
 package com.springcloud.test.system.controller;
 
 import cn.hutool.core.util.RandomUtil;
-import com.springcloud.test.system.entity.Dept;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.stereotype.Component;
 
-import java.util.TreeSet;
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.*;
 
 /**
  * @author 肖龙威
@@ -32,22 +30,22 @@ public class Test {
         return CompletableFuture.supplyAsync(() -> "请求超时");
     }
 
-    public static void main(String[] args) {
-        TreeSet<Dept> set = new TreeSet<Dept>(((o1, o2) -> {
-            return o1.getId().equals(o2.getId()) ? 0 : 1;
-        }));
-        Dept dept = new Dept();
-        dept.setId(1);
-        dept.setName("A");
-        set.add(dept);
-        Dept dept2 = new Dept();
-        dept2.setId(2);
-        dept2.setName("B");
-        set.add(dept2);
-        Dept dept3 = new Dept();
-        dept3.setId(1);
-        dept3.setName("C");
-        set.add(dept3);
-        System.out.println(set);
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+        ExecutorService pool = Executors.newFixedThreadPool(16);
+        //CompletableFuture<Void> hello = CompletableFuture.runAsync(() -> System.out.println("hello"));
+        CompletableFuture<String> cf = CompletableFuture.supplyAsync(() -> {
+            System.out.println("hello world");
+            return "hello world";
+        }, pool);
+
+        CompletableFuture<String> cf2 = cf.exceptionally(e -> {
+            return e.getMessage();
+        });
+
+        CompletableFuture<String> cf1 = cf.thenApplyAsync(String::toUpperCase, pool);
+        //System.out.println(cf.get(3, TimeUnit.SECONDS));
+        //System.out.println(cf2.get());
+        //System.out.println(cf1.get());
+        pool.shutdown();
     }
 }
