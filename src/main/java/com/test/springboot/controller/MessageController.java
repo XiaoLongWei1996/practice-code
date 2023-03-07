b87814688aa29bbad7b8d99025bcc76ea09808b6
肖龙威 <704926476@qq.com>
2023-03-07 17:31:42 +0800

    upd

--- src/main/java/com/test/springboot/controller/MessageController.java
+++ src/main/java/com/test/springboot/controller/MessageController.java
@@ -2,12 +2,16 @@ package com.test.springboot.controller;
 
 
 import com.test.springboot.websocket.WebScoketServer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.UUID;
 
 /**
  * @author 肖龙威
@@ -17,9 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/message01")
 public class MessageController {
 
-
-    @Autowired
-    private ApplicationContext applicationContext;
+    @Resource
+    private RabbitTemplate rabbitTemplate;
 
 
     @GetMapping("/test")
@@ -27,4 +30,13 @@ public class MessageController {
         WebScoketServer.sendMessage(message, "0001");
         return ResponseEntity.ok(200);
     }
+
+    @PostMapping("send")
+    public ResponseEntity<Object> send(String message) {
+        CorrelationData correlationData = new CorrelationData();
+        correlationData.setId(UUID.randomUUID().toString() + "xlw");
+        rabbitTemplate.convertAndSend("ex-delayed", "delay1", message, correlationData);
+        return ResponseEntity.ok(200);
+    }
+
 }
