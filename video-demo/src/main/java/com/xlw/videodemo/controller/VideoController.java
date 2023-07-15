37ac36da9fe8681031152d599437e79585ab4324
肖龙威 <704926476@qq.com>
2023-07-15 17:28:02 +0800

    创建项目

---
+++ video-demo/src/main/java/com/xlw/videodemo/controller/VideoController.java
@@ -0,0 +1,16 @@
+package com.xlw.videodemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @description: 视频处理
+ * @Title: VideoController
+ * @Author xlw
+ * @Package com.xlw.videodemo.controller
+ * @Date 2023/7/15 17:26
+ */
+@RequestMapping("video")
+public class VideoController {
+
+
+}
