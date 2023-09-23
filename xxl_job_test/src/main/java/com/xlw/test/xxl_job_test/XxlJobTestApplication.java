5efa242780d35a23f46ce085fcd484aeffab64b8
肖龙威 <704926476@qq.com>
2023-09-23 16:56:26 +0800

    xxljob

---
+++ xxl_job_test/src/main/java/com/xlw/test/xxl_job_test/XxlJobTestApplication.java
@@ -0,0 +1,13 @@
+package com.xlw.test.xxl_job_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxlJobTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobTestApplication.class, args);
+    }
+
+}
