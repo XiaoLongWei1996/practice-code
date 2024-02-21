78136988d05f17070b075007199d2bb41f9a0e7c
肖龙威 <704926476@qq.com>
2024-02-21 14:52:55 +0800

    upd

---
+++ uploader_demo/src/main/java/com/xlw/test/uploader_demo/UploaderDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xlw.test.uploader_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploaderDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploaderDemoApplication.class, args);
+    }
+
+}
---
+++ uploader_demo/src/test/java/com/xlw/test/uploader_demo/UploaderDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xlw.test.uploader_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploaderDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
