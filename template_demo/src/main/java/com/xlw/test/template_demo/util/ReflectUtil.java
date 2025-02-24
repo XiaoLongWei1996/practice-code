bcb02b129809c7ad04b982f00a12732f6109b4dc
xlw <704926476@qq.com>
2025-02-24 09:28:09 +0800

    upd

--- template_demo/src/main/java/com/xlw/test/template_demo/util/ReflectUtil.java
+++ template_demo/src/main/java/com/xlw/test/template_demo/util/ReflectUtil.java
@@ -2,12 +2,20 @@ package com.xlw.test.template_demo.util;
 
 
 import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
+import org.springframework.asm.ClassReader;
+import org.springframework.asm.ClassVisitor;
+import org.springframework.asm.FieldVisitor;
+import org.springframework.asm.Opcodes;
 
-import java.io.IOException;
-import java.lang.invoke.SerializedLambda;
+import java.lang.invoke.*;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
+import java.util.function.Function;
 
 /**
  * @description: 反射工具类
@@ -17,6 +25,8 @@ import java.lang.reflect.Method;
  * @Date 2025/1/16 10:15
  */
 public class ReflectUtil {
+
+    private static final Map<String, Function> WEAK_MAP = new WeakHashMap<>();
 
     /**
      * 将bean的属性的get/set方法，作为lambda表达式传入时，获取get/set方法对应的属性Field
@@ -69,46 +79,101 @@ public class ReflectUtil {
 
     /**
      * ASM获取字段名称
-     * @param args
-     * @throws IOException
+     *
+     * @param classBytes 类字节
+     * @return {@link List }<{@link String }>
      */
-    //public static List<String> getFieldName(byte[] classBytes) {
-    //    List<String> fields = new ArrayList<>();
-    //    ClassReader reader = new ClassReader(classBytes);
-    //    ClassVisitor visitor = new ClassVisitor(Opcodes.ASM9) {
-    //        @Override
-    //        public FieldVisitor visitField(
-    //                int access,      // 访问修饰符（如public/private）
-    //                String name,    // 字段名
-    //                String descriptor, // 字段类型描述符（如"I"表示int）
-    //                String signature,  // 泛型信息（可为null）
-    //                Object value      // 初始值（常量字段）
-    //        ) {
-    //            // 将字段信息转换为可读字符串
-    //            String fieldInfo = String.format(
-    //                    "字段名: %s, 类型: %s, 修饰符: %s",
-    //                    name, descriptor, access
-    //            );
-    //            fields.add(fieldInfo);
-    //            return super.visitField(access, name, descriptor, signature, value);
-    //        }
-    //    };
-    //    reader.accept(visitor, ClassReader.SKIP_DEBUG);
-    //    return fields;
-    //}
+    public static List<String> getFieldName(byte[] classBytes) {
+        List<String> fields = new ArrayList<>();
+        ClassReader reader = new ClassReader(classBytes);
+        ClassVisitor visitor = new ClassVisitor(Opcodes.ASM9) {
+            @Override
+            public FieldVisitor visitField(
+                    int access,      // 访问修饰符（如public/private）
+                    String name,    // 字段名
+                    String descriptor, // 字段类型描述符（如"I"表示int）
+                    String signature,  // 泛型信息（可为null）
+                    Object value      // 初始值（常量字段）
+            ) {
+                // 将字段信息转换为可读字符串
+                String fieldInfo = String.format(
+                        "字段名: %s, 类型: %s, 修饰符: %s",
+                        name, descriptor, access
+                );
+                fields.add(fieldInfo);
+                return super.visitField(access, name, descriptor, signature, value);
+            }
+        };
+        reader.accept(visitor, ClassReader.SKIP_DEBUG);
+        return fields;
+    }
 
-    public static void main(String[] args) throws IOException {
-        //Student student = new Student();
-        //InputStream inputStream = Student.class.getClassLoader().getResourceAsStream(Student.class.getName().replace(".", "/") + ".class");
-        //
-        //ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        //byte[] buffer = new byte[1024];
-        //int bytesRead;
-        //while ((bytesRead = inputStream.read(buffer)) != -1) {
-        //    baos.write(buffer, 0, bytesRead);
-        //}
-        //byte[] classBytes = baos.toByteArray();
-        //List<String> field = getField(classBytes);
-        //System.out.println(field);
+    /**
+     * 获取字段值
+     *
+     * @param t          t
+     * @param fieldName  字段名称
+     * @param returnType 返回类型
+     * @return {@link R }
+     */
+    public static <T, R> R getFieldValue(T t, String fieldName, Class<R> returnType) {
+        String key = t.getClass().getName() + "." + fieldName;
+        Function<T, R> function = WEAK_MAP.get(key);
+        if (function == null) {
+            try {
+                String methodName = "get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
+                MethodHandles.Lookup lookup = MethodHandles.lookup();
+                MethodHandle methodHandle = lookup.findVirtual(t.getClass(), methodName, MethodType.methodType(returnType));
+                // 创建 Lambda 表达式
+                CallSite callSite = LambdaMetafactory.metafactory(
+                        lookup,
+                        "apply", // 目标方法名
+                        MethodType.methodType(Function.class), // Lambda 类型
+                        MethodType.methodType(Object.class, Object.class), // 函数签名
+                        methodHandle, // 目标方法句柄
+                        methodHandle.type() // 目标方法类型
+                );
+                // 获取 Lambda 实例
+                function = (Function<T, R>) callSite.getTarget().invoke();
+                WEAK_MAP.put(key, function);
+            } catch (Throwable e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return function.apply(t);
+    }
+
+    /**
+     * 获取字段值
+     *
+     * @param t  t
+     * @param fn fn
+     * @return {@link R }
+     */
+    public static <T, R> R getFieldValue(T t, Function<T, R> fn) {
+       return fn.apply(t);
+    }
+
+    public static <T> Object getFieldValue(T t, String fieldName) {
+        Class<?> clazz = t.getClass();
+        try {
+            Field field = clazz.getDeclaredField(fieldName);
+            field.setAccessible(true);
+            return field.get(t);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+       List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        for (String s : list) {
+            list.add(s + "a");
+            System.out.println(s);
+        }
     }
 }
