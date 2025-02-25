65f014470f8f85506d1a0ce43857595f3a6a84b8
xlw <704926476@qq.com>
2025-02-25 15:25:42 +0800

    upd

--- template_demo/src/main/java/com/xlw/test/template_demo/util/CompareUtil.java
+++ template_demo/src/main/java/com/xlw/test/template_demo/util/CompareUtil.java
@@ -3,12 +3,11 @@ package com.xlw.test.template_demo.util;
 
 import cn.hutool.core.lang.Assert;
 import com.xlw.test.template_demo.entity.Student;
-import com.xlw.test.template_demo.entity.User;
 import lombok.NonNull;
 
-import java.time.LocalDateTime;
 import java.util.*;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * @description: 比较工具类
@@ -26,10 +25,10 @@ public class CompareUtil {
      *
      * @param bean1 豆 1
      * @param bean2 豆 2
-     * @return {@link FieldEqualsBuilder }<{@link T1 }, {@link T2 }>
+     * @return {@link FieldEqualsHandler }<{@link T1 }, {@link T2 }>
      */
-    public static <T1, T2> FieldEqualsBuilder<T1, T2> fieldComparatorBuild(@NonNull T1 bean1, @NonNull T2 bean2) {
-        return new FieldEqualsBuilder<T1, T2>(bean1, bean2);
+    public static <T1, T2> FieldEqualsHandler<T1, T2> fieldEqualsGenerator(@NonNull T1 bean1, @NonNull T2 bean2) {
+        return new FieldEqualsHandler<T1, T2>(bean1, bean2);
     }
 
     /**
@@ -44,12 +43,70 @@ public class CompareUtil {
     }
 
     /**
-     * 字段等于生成器
+     * 不等于执行
+     *
+     * @param o1
+     * @param o2               O2
+     * @param notEqualsExecute 不等于执行
+     * @return {@link Optional }<{@link R }>
+     */
+    public static <T1, T2, R> Optional<R> notEqualsExecute(@NonNull T1 o1, @NonNull T2 o2, @NonNull Supplier<R> notEqualsExecute) {
+        Assert.notNull(o1, "对象为空");
+        Assert.notNull(o2, "对象为空");
+        return Objects.equals(o1, o2) ? Optional.empty() : Optional.ofNullable(notEqualsExecute.get());
+    }
+
+    /**
+     * 不等于执行
+     *
+     * @param o1
+     * @param o2               O2
+     * @param notEqualsExecute 不等于执行
+     */
+    public static <T1, T2> void notEqualsExecute(@NonNull T1 o1, @NonNull T2 o2, @NonNull Runnable notEqualsExecute) {
+        Assert.notNull(o1, "对象为空");
+        Assert.notNull(o2, "对象为空");
+        if (!Objects.equals(o1, o2)) {
+            notEqualsExecute.run();
+        }
+    }
+
+    /**
+     * 等于执行
+     *
+     * @param o1            o1
+     * @param o2            o2
+     * @param equalsExecute 等于执行
+     * @return {@link Optional }<{@link R }>
+     */
+    public static <T1, T2, R> Optional<R> equalsExecute(@NonNull T1 o1, @NonNull T2 o2, @NonNull Supplier<R> equalsExecute) {
+        Assert.notNull(o1, "对象为空");
+        Assert.notNull(o2, "对象为空");
+        return Objects.equals(o1, o2) ? Optional.ofNullable(equalsExecute.get()) : Optional.empty();
+    }
+
+    /**
+     * 等于执行
+     *
+     * @param o1            o1
+     * @param o2            o2
+     * @param equalsExecute 等于执行
+     */
+    public static <T1, T2> void equalsExecute(@NonNull T1 o1, @NonNull T2 o2, @NonNull Runnable equalsExecute) {
+        Assert.notNull(o1, "对象为空");
+        Assert.notNull(o2, "对象为空");
+        if (Objects.equals(o1, o2)) {
+            equalsExecute.run();
+        }
+    }
+
+    /**
+     * 字段等于处理程序
      *
      * @author xlw
      * @date 2025/02/22
      */
-    private static class FieldEqualsBuilder<T1, T2> {
+    public static class FieldEqualsHandler<T1, T2> {
 
         private T1 t1;
 
@@ -57,29 +114,28 @@ public class CompareUtil {
 
         private List<EqualsObject> eqs = new ArrayList<>();
 
-        private FieldEqualsBuilder(T1 t1, T2 t2) {
+        private FieldEqualsHandler(T1 t1, T2 t2) {
             this.t1 = t1;
             this.t2 = t2;
         }
 
-        public final FieldEqualsBuilder<T1, T2> fieldGroup(Function<T1, ?> filedFn1, Function<T2, ?> filedFn2, String errorMsg) {
+        public final FieldEqualsHandler<T1, T2> fieldGroup(Function<T1, ?> filedFn1, Function<T2, ?> filedFn2, String errorMsg) {
             eqs.add(new EqualsObject(filedFn1.apply(t1), filedFn2.apply(t2), errorMsg));
             return this;
         }
 
-        public final FieldEqualsBuilder<T1, T2> fieldGroup(Function<T1, ?> filedFn1, Function<T2, ?> filedFn2) {
+        public final FieldEqualsHandler<T1, T2> fieldGroup(Function<T1, ?> filedFn1, Function<T2, ?> filedFn2) {
             return fieldGroup(filedFn1, filedFn2,DEFAULT_ERROR_MSG);
         }
 
         public boolean isEquals() {
             Assert.notEmpty(eqs, "未找到可比对的字段信息");
-            boolean flag = true;
             for (EqualsObject eq : eqs) {
                 if (!eq.equals()) {
-                    flag = false;
+                    return false;
                 }
             }
-            return flag;
+            return true;
         }
 
         /**
@@ -98,6 +154,48 @@ public class CompareUtil {
             return msgSet;
         }
 
+        /**
+         * 等于执行
+         *
+         * @param equalsExecute 等于执行
+         */
+        public void equalsExecute(@NonNull Runnable equalsExecute) {
+            if (isEquals()) {
+                equalsExecute.run();
+            }
+        }
+
+        /**
+         * 等于执行
+         *
+         * @param equalsExecute 等于执行
+         * @return {@link Optional }<{@link R }>
+         */
+        public <R> Optional<R> equalsExecute(@NonNull Supplier<R> equalsExecute) {
+            return isEquals() ? Optional.ofNullable(equalsExecute.get()) : Optional.empty();
+        }
+
+        /**
+         * 不等于执行
+         *
+         * @param notEqualsExecute 不等于执行
+         */
+        public void notEqualsExecute(@NonNull Runnable notEqualsExecute) {
+            if (!isEquals()) {
+                notEqualsExecute.run();
+            }
+        }
+
+        /**
+         * 不等于执行
+         *
+         * @param notEqualsExecute 不等于执行
+         * @return {@link Optional }<{@link R }>
+         */
+        public <R> Optional<R> notEqualsExecute(@NonNull Supplier<R> notEqualsExecute) {
+            return !isEquals() ? Optional.ofNullable(notEqualsExecute.get()) : Optional.empty();
+        }
+
     }
 
     /**
@@ -106,7 +204,7 @@ public class CompareUtil {
      * @author xlw
      * @date 2025/02/22
      */
-    private static class EqualsObject {
+    public static class EqualsObject {
 
         private Object o1;
 
@@ -134,14 +232,12 @@ public class CompareUtil {
 
     public static void main(String[] args) {
         Student student = new Student();
-        student.setName("张伟");
-        student.setBirthday(LocalDateTime.now());
-        User user = new User();
-        user.setName("张伟");
-        Set<String> msg = CompareUtil.fieldComparatorBuild(student, user)
-                .fieldGroup(Student::getName, User::getName)
-                .fieldGroup(Student::getBirthday, User::getName, "姓名不一致")
-                .isEqualsReturnMsg();
-        System.out.println(msg);
+        student.setName("张三");
+        Student student1 = new Student();
+        student1.setName("张三1");
+        Optional<Integer> i = CompareUtil.fieldEqualsGenerator(student, student1)
+                .fieldGroup(Student::getName, Student::getName)
+                .notEqualsExecute(() -> 123);
+        System.out.println(i.get());
     }
 }
