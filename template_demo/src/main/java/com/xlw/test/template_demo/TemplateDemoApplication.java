4297dd81e03d446219612cbc6eb5d70be084a128
xlw <704926476@qq.com>
2025-03-17 18:26:33 +0800

    日志追踪

--- template_demo/src/main/java/com/xlw/test/template_demo/TemplateDemoApplication.java
+++ template_demo/src/main/java/com/xlw/test/template_demo/TemplateDemoApplication.java
@@ -3,11 +3,13 @@ package com.xlw.test.template_demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
+@ServletComponentScan(basePackages = "com.xlw.test.template_demo.config")
 @EnableScheduling
 @EnableRetry
 @MapperScan
