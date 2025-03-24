3dd0234de789a997498f15df9f877129196df7e0
xlw <704926476@qq.com>
2025-03-24 19:03:48 +0800

    upd

---
+++ mapstruct_demo/src/main/java/com/xlw/mapstruct_demo/MapstructDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xlw.mapstruct_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapstructDemoApplication.class, args);
+    }
+
+}
