b20a67a95b7466501fa21445d85e3e09203274ac
xlw <704926476@qq.com>
2025-03-24 19:06:26 +0800

    upd

--- log4j2_demo/src/main/java/com/xlw/test/log4j2_test/Log4j2TestApplication.java
+++ log4j2_demo/src/main/java/com/xlw/test/log4j2_test/Log4j2TestApplication.java
@@ -2,7 +2,9 @@ package com.xlw.test.log4j2_test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan(basePackages = "com.xlw.test.log4j2_test.config")
 @SpringBootApplication
 public class Log4j2TestApplication {
 
---
+++ log4j2_demo/src/main/java/com/xlw/test/log4j2_test/config/TraceIdFilter.java
@@ -0,0 +1,60 @@
+package com.xlw.test.log4j2_test.config;
+
+
+import cn.hutool.core.util.IdUtil;
+import org.apache.logging.log4j.ThreadContext;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author xlw
+ * @description: 日志跟踪过滤器
+ * @title: TraceIdFilter
+ * @package com.xlw.test.template_demo.config
+ * @date 2025/3/17 18:01
+ */
+@WebFilter(filterName = "traceIdFilter", urlPatterns = "/*")
+public class TraceIdFilter implements Filter {
+
+    private static final String TRACE_ID = "traceId";
+
+    /**
+     * 初始化
+     *
+     * @param filterConfig 过滤器配置
+     * @throws ServletException Servlet 异常
+     */
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        // 生成一个随机数给到前端
+        String traceId = IdUtil.fastSimpleUUID();
+        try {
+            // 随机数放到此线程的上下文中，可以在每条日志前加入。具体看下面log4j2.xml
+            ThreadContext.put(TRACE_ID, traceId);
+            // 随机数放到Header中，在ResponseHeaders中可查看到此数据
+            resp.addHeader(TRACE_ID, traceId);
+            filterChain.doFilter(req, resp);
+        } finally {
+            ThreadContext.clearAll();
+        }
+    }
+
+    /**
+     * 摧毁
+     */
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
