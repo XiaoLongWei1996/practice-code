dbefc0d7da7d99adbc7be157e54a9e81ddebbc77
xlw <704926476@qq.com>
2025-03-26 13:58:04 +0800

    upd

--- mapstruct_demo/src/main/java/com/xlw/mapstruct_demo/util/FastExcelUtil.java
+++ mapstruct_demo/src/main/java/com/xlw/mapstruct_demo/util/FastExcelUtil.java
@@ -1,6 +1,14 @@
 package com.xlw.mapstruct_demo.util;
 
 
+import cn.idev.excel.FastExcel;
+import cn.idev.excel.read.listener.ReadListener;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author xlw
  * @description: fast excel 工具类
@@ -9,4 +17,77 @@ package com.xlw.mapstruct_demo.util;
  * @date 2025/3/25 20:02
  */
 public class FastExcelUtil {
+
+    /**
+     * 一次读取Excel
+     *
+     * @param filePath    文件路径
+     * @param headerClass header 类
+     * @param sheetIndex  Sheet Index （工作表索引）
+     * @return {@link List }<{@link T }>
+     */
+    public static <T> List<T> readExcel(String filePath, Class<T> headerClass, int sheetIndex, int headerIndex) {
+        return FastExcel.read(filePath).head(headerClass).sheet(sheetIndex).headRowNumber(headerIndex).doReadSync();
+    }
+
+    /**
+     * 阅读 Excel
+     *
+     * @param inputStream 输入流
+     * @param headerClass header 类
+     * @param sheetIndex  Sheet Index （工作表索引）
+     * @param headerIndex 头部索引
+     * @return {@link List }<{@link T }>
+     */
+    public static <T> List<T> readExcel(InputStream inputStream, Class<T> headerClass, int sheetIndex, int headerIndex) {
+        return FastExcel.read(inputStream).head(headerClass).sheet(sheetIndex).headRowNumber(headerIndex).doReadSync();
+    }
+
+    /**
+     * 阅读 Excel
+     *
+     * @param filePath    文件路径
+     * @param sheetIndex  Sheet Index （工作表索引）
+     * @param headerIndex 标头索引
+     * @return {@link List }<{@link Map }<{@link String }, {@link Object }>>
+     */
+    public static List<Map<String, Object>> readExcel(String filePath, int sheetIndex, int headerIndex) {
+        return FastExcel.read(filePath).sheet(sheetIndex).headRowNumber(headerIndex).doReadSync();
+    }
+
+    /**
+     * 阅读 Excel
+     *
+     * @param filePath     文件路径
+     * @param sheetIndex   Sheet Index （工作表索引）
+     * @param headerIndex  标头索引
+     * @param readListener 读取侦听器
+     */
+    public static <T> void readExcel(String filePath, int sheetIndex, int headerIndex, ReadListener<T> readListener) {
+        FastExcel.read(filePath, readListener).sheet(sheetIndex).headRowNumber(headerIndex).doRead();
+    }
+
+    /**
+     * 编写 Excel
+     *
+     * @param filePath    文件路径
+     * @param sheetName   工作表名称
+     * @param data        数据
+     * @param headerClass header 类
+     */
+    public static <T> void writeExcel(String filePath, String sheetName, List<T> data, Class<T> headerClass) {
+        FastExcel.write(filePath).head(headerClass).sheet(sheetName).doWrite(data);
+    }
+
+    /**
+     * 编写 Excel
+     *
+     * @param outputStream 输出流
+     * @param sheetName    工作表名称
+     * @param data         数据
+     */
+    public static <T> void writeExcel(OutputStream outputStream, String sheetName, List<T> data) {
+        FastExcel.write(outputStream).sheet(sheetName).doWrite(data);
+    }
+
 }
